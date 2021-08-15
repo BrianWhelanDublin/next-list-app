944f94e98e59e17f0709eecfa0ff9bea1d0fd5a5
Brian Whelan <brian@friday.ie>
2021-08-15 12:18:16 +0100

    finish project

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer"
+
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe mollitia illum voluptatum voluptas dolore architecto doloremque, cum ab tempore quaerat tempora autem doloribus dicta minima quod itaque enim voluptatem.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet animi sequi similique, consectetur porro non quae officiis laudantium, quibusdam numquam obcaecati quasi perspiciatis tempore quaerat odio aut odit incidunt blanditiis!</p>
+     
+        </div>
+     );
+}
+ 
+export default About;
